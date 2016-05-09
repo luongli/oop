84ac2dad3f5d94e8535bac7096d6c4fd79ae3ab3
Nguyen Van Luong <luongintel2009@gmail.com>
2016-05-09 14:37:47 +0700

    Add: Date class

---
+++ src/li/com/Date.java
@@ -0,0 +1,37 @@
+package li.com;
+
+public class Date {
+	private int day;
+	private int month;
+	private int year;
+	
+	// getter for day
+	public int getDay() {
+		return day;
+	}
+	
+	// setter for day
+	public void setDay(int day) {
+		this.day = day;
+	}
+	
+	// getter for month
+	public int getMonth() {
+		return month;
+	}
+	
+	// setter for month
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	
+	// getter for year
+	public int getYear() {
+		return year;
+	}
+	
+	// setter for year
+	public void setYear(int year) {
+		this.year = year;
+	}
+}
